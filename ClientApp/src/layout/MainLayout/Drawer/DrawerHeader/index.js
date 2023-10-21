c7734da6f293c04c5225fb6b336c83f28226ac96
Aubrey Yates <aubreyyates999@hotmail.com>
2023-10-21 10:28:18 -0500

    Change Logo and text used in MainLayout.

--- ClientApp/src/layout/MainLayout/Drawer/DrawerHeader/index.js
+++ ClientApp/src/layout/MainLayout/Drawer/DrawerHeader/index.js
@@ -1,12 +1,13 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 // material-ui
-import { useTheme } from '@mui/material/styles';
-import { Stack, Chip } from '@mui/material';
+import { useTheme } from "@mui/material/styles";
+import { Stack, Chip } from "@mui/material";
 
 // project import
-import DrawerHeaderStyled from './DrawerHeaderStyled';
-import Logo from 'components/Logo';
+import DrawerHeaderStyled from "./DrawerHeaderStyled";
+import Logo from "components/Logo";
+import Typography from "themes/overrides/Typography";
 
 // ==============================|| DRAWER HEADER ||============================== //
 
@@ -18,22 +19,14 @@ const DrawerHeader = ({ open }) => {
     <DrawerHeaderStyled theme={theme} open={open}>
       <Stack direction="row" spacing={1} alignItems="center">
         <Logo />
-        <Chip
-          label={process.env.REACT_APP_VERSION}
-          size="small"
-          sx={{ height: 16, '& .MuiChip-label': { fontSize: '0.625rem', py: 0.25 } }}
-          component="a"
-          href="https://github.com/codedthemes/mantis-free-react-admin-template"
-          target="_blank"
-          clickable
-        />
+        <h5>Project App</h5>
       </Stack>
     </DrawerHeaderStyled>
   );
 };
 
 DrawerHeader.propTypes = {
-  open: PropTypes.bool
+  open: PropTypes.bool,
 };
 
 export default DrawerHeader;
